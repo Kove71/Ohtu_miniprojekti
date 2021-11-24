b6bdac078d8d420023e0d82c4d5b6926f1636ac8
Kosti Vesterinen <kosti.vesterinen@helsinki.fi>
2021-11-24 22:17:43 +0200

    invoke ja invoke-toiminnot lisätty

---
+++ tasks.py
@@ -0,0 +1,17 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py")
+
+@task
+def test(ctx):
+    ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src; coverage html")
+
+@task
+def lint(ctx):
+    ctx.run("pylint src")
