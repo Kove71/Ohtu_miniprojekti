17dd11b10960ee7e6789f52ac9b2ef319125f417
Reima Kuomanen <reimkuos@gmail.com>
2021-11-28 16:00:16 +0200

    created tests for class ReadingTip

---
+++ src/tests/reading_tips_test.py
@@ -0,0 +1,13 @@
+"""Tests for the class ReadingTip 
+"""
+import unittest
+from readingtips.readingtip import ReadingTip
+
+class TestTip(unittest.TestCase):
+
+    def test_the_commetnt_is_saveds_as_is(self):
+        """Tests if the class holds the string given to it as
+        input 
+        """
+        tip = ReadingTip("Hello World")
+        self.assertEqual("Hello Wolrd", tip.__str__())
