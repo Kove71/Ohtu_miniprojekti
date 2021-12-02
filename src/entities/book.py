ba7c1fca0a09c5c481bb41bf2c77f74002bc2ed7
sofiaaair <sofia.airas@hotmail.com>
2021-12-02 23:49:10 +0200

    luokka kirjalle

---
+++ src/entities/book.py
@@ -0,0 +1,16 @@
+"""Luokka kirjaoliolle
+"""
+
+class Book:
+    """Kirja
+    """
+    def __init__(self, name, author, isbn=None, description=None):
+        """Kirjan konstruktori
+        """
+        self.name = name
+        self.author = author
+        self.isbn = isbn or None
+        self.description = description or None
+
+    def __str__(self):
+        return self.name + " " + self.author + " " + self.isbn
