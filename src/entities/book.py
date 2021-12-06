4f32b856ac014839c0c297a8eb45f8ed8baf55fe
Reima Kuomanen <reimkuos@gmail.com>
2021-12-06 02:35:09 +0200

    Paivitetty book luokka

--- src/entities/book.py
+++ src/entities/book.py
@@ -4,13 +4,25 @@
 class Book:
     """Kirja
     """
-    def __init__(self, name, author, isbn=None, description=None):
+    def __init__(self, name, author, isbn=None, description=None, read=False):
         """Kirjan konstruktori
         """
         self.name = name
         self.author = author
         self.isbn = isbn or None
         self.description = description or None
+        
+        self.read = read
+        self._type = 1
+
+    def type(self):
+        return self._type
 
     def __str__(self):
-        return f'{self.author}: "{self.name}"'
+        string = f"Type: Book\n" \
+                f"Name: {self.name}\n" \
+                f"Author: {self.author}\n" \
+                f"ISBN: {self.isbn if self.isbn is not None else 'Unknown'}\n" \
+                f"Description: {self.description if self.description is not None else 'None'}\n" \
+                f"{'Already read' if self.read else 'Not Read'}\n"        
+        return string
