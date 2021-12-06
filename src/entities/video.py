c75eb53bc08e9271b66d18f928ea84b6017f01f0
Reima Kuomanen <reimkuos@gmail.com>
2021-12-06 02:36:14 +0200

    Paivitetty video luokka

--- src/entities/video.py
+++ src/entities/video.py
@@ -4,7 +4,7 @@
 class Video:
     """Video
     """
-    def __init__(self, name, url, channel=None, description=None):
+    def __init__(self, name, url, channel=None, description=None, read=False):
         """Videon konstruktori
         """
         self.name = name
@@ -12,5 +12,18 @@ class Video:
         self.channel = channel
         self.description = description
 
+        self.read = read
+        self._type = 4
+
+    def type(self):
+        return self._type
+
     def __str__(self):
-        return f'"{self.name}", url: {self.url}"'
+        string = f"Type: Video\n" \
+                f"Name: {self.name}\n" \
+                f"URL: {self.url}\n" \
+                f"Channel: {self.channel if self.channel is not None else 'Unknown'}\n" \
+                f"Description: {self.description if self.description is not None else 'None'}\n" \
+                f"{'Already watched' if self.read else 'Not watched'}\n"   
+        return string
+        
