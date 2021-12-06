c9499dfc555fc26baa9cc83c2a1650358cb8b58f
Reima Kuomanen <reimkuos@gmail.com>
2021-12-06 02:35:26 +0200

    Paivitetty podcast luokka

--- src/entities/podcast.py
+++ src/entities/podcast.py
@@ -4,7 +4,7 @@
 class Podcast:
     """Podcast
     """
-    def __init__(self, name, episode, url=None, description=None):
+    def __init__(self, name, episode, url=None, description=None, read=False):
         """Podcastin konstruktori
         """
         self.name = name
@@ -12,6 +12,18 @@ class Podcast:
         self.url = url
         self.description = description
 
+        self.read = read
+        self._type = 3
+
+    def type(self):
+        return self._type
+
     def __str__(self):
-        return f'{self.name} - {self.episode}'
+        string = f"Type: Podcast\n" \
+                f"Name: {self.name}\n" \
+                f"Episode: {self.author}\n" \
+                f"URL: {self.url if self.url is not None else 'Unknown'}\n" \
+                f"Description: {self.description if self.description is not None else 'None'}\n" \
+                f"{'Already listened' if self.read else 'Not Listened'}\n"   
+        return string
         
