5875999f9e6f49f364ac5359a2c237563aaf6174
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2021-12-11 14:45:57 +0200

    robot insertion test toimii

--- src/ReadingtipLibrary.py
+++ src/ReadingtipLibrary.py
@@ -8,6 +8,7 @@ from ui.ui import UI
 from repositories.db_interface import DatabaseInterface
 from db_build import build_database
 from db_clear import clear_database
+from services.readingtip_service import ReadingtipService
 class ReadingtipLibrary: # pylint: disable=invalid-name
     """Luokka joka vastaa vaatimusten testaamisesta
     """
@@ -20,9 +21,10 @@ class ReadingtipLibrary: # pylint: disable=invalid-name
         clear_database(self.db_name)
         build_database(self.db_name)
         self.database = DatabaseInterface(self.db_name)
-        
-        self._ui = UI(self._io, self.database)
+        self.service = ReadingtipService(self.database)
 
+        self._ui = UI(self._io, self.service)
+        
 
     def input(self, value):
         """Luo syötteen
@@ -33,7 +35,6 @@ class ReadingtipLibrary: # pylint: disable=invalid-name
         """Tarkistaa tulosteen
         """
         outputs = self._io.outputs
-
         if not value in outputs:
             raise AssertionError(
                 f"Output \"{value}\" is not in {str(outputs)}"
