46a6277fb0efdd6e9a22b534eb23ea030185bacc
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2021-12-11 10:47:44 +0200

    db_conectionille voi antaa vapaavalintaisen polun ja nimen tietokannalle (testitarkoituksiin)

--- src/db_connection.py
+++ src/db_connection.py
@@ -3,10 +3,15 @@
 import sqlite3
 from config import DATABASE_FILE_PATH
 
-connection = sqlite3.connect(DATABASE_FILE_PATH)
-connection.isolation_level = None
+CONNECTION = sqlite3.connect(DATABASE_FILE_PATH)
+CONNECTION.isolation_level = None
 
-def get_connection():
+def get_connection(database_path = None):
     """Hakee yhteyden tietokantaan ja palauttaa sen.
     """
-    return connection
+    if not database_path:
+        return CONNECTION
+    else:
+        alt_connection = sqlite3.connect(database_path)
+        alt_connection.isolation_level = None
+        return alt_connection
