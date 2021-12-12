37cfa64229771d7d275e86da21ea5ef3b97d06fa
sainigma <sainigma@gmail.com>
2021-12-12 20:19:50 +0200

    fixed comment saved as is test for reading tips test

--- src/tests/reading_tips_test.py
+++ src/tests/reading_tips_test.py
@@ -6,11 +6,11 @@ from entities.readingtip import ReadingTip
 class TestTip(unittest.TestCase):
     """Testausluokka ReadintTip-luokalle
     """
-    def test_the_commetnt_is_saveds_as_is(self):
+    def test_the_comment_is_saved_as_is(self):
         """Testaa säilyttääkö luokka sille annetun syötteen.
         """
         tip = ReadingTip("Hello World")
-        self.assertEqual("Hello World", tip.__str__())
+        self.assertEqual("Hello World\n", tip.__str__())
 
     def test_base_class_returns_none_type(self):
         """Testaa palauttaako luokka tyyppinään None
