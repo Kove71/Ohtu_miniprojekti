0929828018de827675e645d116cd40bebd454e09
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2021-12-13 10:37:19 +0200

    korjattu blogin ja videon tulostusasusta ISBN pois

--- src/entities/blog.py
+++ src/entities/blog.py
@@ -21,7 +21,7 @@ class Blog:
         string = f"\033[1;37;40mBlog {self.id: <72}" \
                 f"\033[0;31;47m\nName: \033[0;30;47m{self.name: <50}" \
                 f"\033[1;34;0m\nAuthor: {self.author}\n" \
-                f"ISBN: {self.title if self.title is not None else 'Unknown'}\n" \
+                f"Blogpost title: {self.title if self.title is not None else 'Unknown'}\n" \
                 f"URL: {self.url}\n" \
                 f"Description: {self.description if self.description is not None else 'None'}\033[0;0;0m\n" \
                 f"Read: {'Not read' if self.read is None else self.read}\n"
